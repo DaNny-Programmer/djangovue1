88d4930904a569282a9d178af8ca8382f3961a11
Daniela Muñoz <daniimhdez08@gmail.com>
2020-06-20 11:25:15 -0500

    Crear resto de los recursos -  modelos Category & Type

--- listelement/urls.py
+++ listelement/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework import routers
-from .viewsets import ElementViewSet
+from .viewsets import ElementViewSet, CategoryViewSet, TypeViewSet
 
 route = routers.SimpleRouter()
 route.register('Element', ElementViewSet)
+route.register('Category', CategoryViewSet)
+route.register('Type', TypeViewSet)
 
 
 urlpatterns = route.urls
